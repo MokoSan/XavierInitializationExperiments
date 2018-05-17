1df4bdd87ef4ec647625ab5ee790257c20d10ce0
anmolsjoshi <anmolsjoshi@gmail.com>
2018-05-16 20:31:51 -0700

    Updated code for create_data.py and model.py

---
+++ src/data/create_data.py
@@ -0,0 +1,189 @@
+from tensorflow.python.keras.datasets import mnist, cifar10
+from tensorflow.python.keras.utils import to_categorical
+from Shapeset.curridata import *
+from Shapeset.buildfeaturespolygon import *
+from Shapeset.polygongen import *
+import numpy as np
+import itertools
+
+
+class CreateDataset:
+
+    """
+    CreateDataset class contains three methods mnist, cifar10, shapeset to create respective datasets.
+    - mnist, cifar10 methods take advantage of Keras pre-built data functions
+    - shapeset method uses the Shapeset API created by glorotxa
+
+    The methods of this class return the training and validation sets for each dataset. The training set can be
+    numpy arrays of features and classes, or a generator that yeilds features and classes.
+
+    Use getattr function to assign a flag and call a method of this class
+    """
+
+    def mnist(self):
+        """
+        Returns:
+            A tuple of training and validation sets of the MNIST dataset.
+            - x_train: flattened training set, mean subtracted and standardized
+            - y_train: one hot encoded training classes
+            - x_val: flattened validation set, mean subtracted and standardized using the training set
+            - y_val: one hot encoded validation classes
+        """
+
+        (x_train, y_train), (x_val, y_val) = mnist.load_data()
+
+        x_mean = x_train.mean()
+        x_std = x_train.std()
+
+        x_train = x_train - x_mean
+        x_train /= x_std
+        x_train = x_train.reshape(x_train.shape[0], -1)
+        y_train = to_categorical(y_train, 10)
+
+        x_val = x_val - x_mean
+        x_val /= x_std
+        x_val = x_val.reshape(x_val.shape[0], -1)
+        y_val = to_categorical(y_val, 10)
+
+        return x_train, y_train, x_val, y_val
+
+    def cifar10(self):
+        """
+        Returns:
+            A tuple of training and validation sets of the MNIST dataset.
+            - x_train: flattened training set, mean subtracted and standardized
+            - y_train: one hot encoded training classes
+            - x_val: flattened validation set, mean subtracted and standardized using the training set
+            - y_val: one hot encoded validation classes
+        """
+        (x_train, y_train), (x_val, y_val) = cifar10.load_data()
+
+        x_mean = x_train.mean()
+        x_std = x_train.std()
+
+        x_train = x_train - x_mean
+        x_train /= x_std
+        x_train = x_train.reshape(x_train.shape[0], -1)
+        y_train = to_categorical(y_train, 10)
+
+        x_val = x_val - x_mean
+        x_val /= x_std
+        x_val = x_val.reshape(x_val.shape[0], -1)
+        y_val = to_categorical(y_val, 10)
+
+        return x_train, y_train, x_val, y_val
+
+    def shapeset(self):
+        """
+        Returns:
+            A generator that yeilds batches of size 10, of features and one hot encoded classes, and a validation set of
+            300 examples
+            - shapeset_generator: Infinite generator that yeilds batches of features and classes, batch size = 10
+            - x_val: numpy array of features of 300 examples
+            - y_val: numpy array of one hot encoded classes of 300 examples
+        """
+
+        genparams = {'inv_chance': 0.5, 'img_shape': (32, 32), 'n_vert_list': [3, 4, 20], 'fg_min': 0.55, 'fg_max': 1.0,
+                     'bg_min': 0.0, 'bg_max': 0.45, 'rot_min': 0.0, 'rot_max': 1, 'pos_min': 0, 'pos_max': 1,
+                     'scale_min': 0.2, 'scale_max': 0.8, 'rotation_resolution': 255, 'nb_poly_max': 2, 'nb_poly_min': 1,
+                     'overlap_max': 0.5, 'poly_type': 2, 'rejectionmax': 50, 'overlap_bool': True}
+
+        datagenerator = Polygongen
+        funclist = [buildimage, buildedgesangle, builddepthmap, buildidentity, buildsegmentation, output,
+                    buildedgesanglec]
+        dependencies = [None, {'segmentation': 4}, None, None, {'depthmap': 2}, None, {'segmentation': 4}]
+        funcparams = {'neighbor': 'V8', 'gaussfiltbool': False, 'sigma': 0.5, 'size': 5}
+        nfeatures = 6
+
+        batchsize = 10
+        seed = 0
+        funcparams.update({'neg': True})
+
+        curridata = Curridata(nfeatures, datagenerator, genparams, funclist, dependencies, funcparams, batchsize, seed)
+
+        shapeset_generator = self.generator(data_generator=curridata, batch_size=batchsize)
+        x_val, y_val = self.create_validation_set(data_generator=curridata)
+
+        return shapeset_generator, x_val, y_val
+
+    def generator(self, data_generator, batch_size):
+        """
+        Creates generator of batches of Shapeset features and labels, of batch size 10
+
+        Input:
+            - data_generator: curridata generator using the Shapeset API
+            - batch_size: number of batches to be generated
+
+        Returns:
+            - batch_feautures: flattened batches of Shapeset features as numpy arrays
+            - batch_labels: batches of one hot encoded Shapeset classes as numpy arrays
+        """
+
+        batch_features = np.zeros((batch_size, 1024))
+        batch_labels = np.zeros((batch_size, 9))
+
+        while True:
+            data_generator.next()
+            data_generator.image
+
+            batch_features = data_generator._Curridata__features[0]
+            batch_labels = self.convertout(data_generator.output)
+            batch_labels = to_categorical(batch_labels, 9)
+
+            yield batch_features, batch_labels
+
+    def convertout(self, out):
+
+        target = 0 * ((out[:, 0] == 1) * (out[:, 1] == 0) * (out[:, 2] == 0)) + \
+                 1 * ((out[:, 0] == 0) * (out[:, 1] == 1) * (out[:, 2] == 0)) + \
+                 2 * ((out[:, 0] == 0) * (out[:, 1] == 0) * (out[:, 2] == 1)) + \
+                 3 * ((out[:, 0] == 1) * (out[:, 1] == 1) * (out[:, 2] == 0)) + \
+                 4 * ((out[:, 0] == 0) * (out[:, 1] == 1) * (out[:, 2] == 1)) + \
+                 5 * ((out[:, 0] == 1) * (out[:, 1] == 0) * (out[:, 2] == 1)) + \
+                 6 * ((out[:, 0] == 2) * (out[:, 1] == 0) * (out[:, 2] == 0)) + \
+                 7 * ((out[:, 0] == 0) * (out[:, 1] == 2) * (out[:, 2] == 0)) + \
+                 8 * ((out[:, 0] == 0) * (out[:, 1] == 0) * (out[:, 2] == 2))
+
+        return target
+
+
+    def create_validation_set(self, data_generator):
+        """
+        Creates validaiton set by generator 30 batches of examples
+
+        Input:
+            - data_generator: curridata generator using the Shapeset API
+
+        Returns:
+             - x_val: validation set of flattened Shapeset features as numpy arrays
+             - y_val: validation set of one hot encoded Shapeset classes as numpy arrays
+        """
+
+        x_val, y_val = [], []
+
+        for _ in range(30):
+            data_generator.next()
+            data_generator.image
+
+            x_val.append(data_generator._Curridata__features[0])
+            y_val.append(to_categorical(self.convertout(data_generator.output), 9))
+
+        x_val = list(itertools.chain(*x_val))
+        y_val = list(itertools.chain(*y_val))
+
+        return np.array(x_val), np.array(y_val)
+
+
+if __name__ == '__main__':
+
+    data = CreateDataset()
+    flag = 'mnist'
+    create_data_method = getattr(data, flag)
+
+    if flag == 'shapeset':
+        x_gen, x_val, y_val = create_data_method()
+        x_train, y_train = next(x_gen)
+    else:
+        x_train, y_train, x_val, y_val = create_data_method()
+
+    print (x_train.shape, y_train.shape, x_val.shape, y_val.shape)
---
+++ src/models/model.py
@@ -0,0 +1,43 @@
+from tensorflow.python.keras.initializers import RandomUniform, glorot_uniform
+from tensorflow.python.keras.layers import Input, Dense
+from tensorflow.python.keras.models import Model
+import numpy as np
+
+def FullyConnectedNN(input_shape, classes, five_layers, activation, normalization):
+
+    """
+    Creates 4 or 5 Layer Fully Connected Neural Network
+
+    Input:
+        - input_shape: int, shape of features of examples
+        - classes: int, number of classes
+        - activation: activation function for layers - 'tanh', 'sigmoid', 'softsign'
+        - normalization: boolean, True for Glorot Initialization, False for Random Uniform Initialization
+        - five_layers: boolean, True for 5 layer network, False for 4 layer network
+
+    Returns:
+        - model: model class created with specified inputs
+
+    """
+    w_in = np.sqrt(0.001)
+    initializer = RandomUniform(minval=-w_in, maxval=w_in)
+
+    if normalization:
+        initializer = glorot_uniform()
+
+    x_input = Input(shape=(input_shape,))
+    x = Dense(units=1000, activation=activation, kernel_initializer=initializer)(x_input)
+
+    num_layers = 4
+
+    if five_layers:
+        num_layers = 5
+
+    for _ in range(num_layers - 2):
+        x = Dense(units=1000, activation=activation, kernel_initializer=initializer)(x)
+
+    x_output = Dense(units=classes, activation='sotfmax', kernel_initializer=initializer)(x)
+
+    model = Model(inputs=x_input, outputs=x_output)
+
+    return model
