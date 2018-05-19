10b36ab3530c80293efbcbaac47531b3e914599b
Anmol Joshi <anmolsjoshi@gmail.com>
2018-05-19 07:07:23 -0700

    Trial install.sh

--- install.sh
+++ install.sh
@@ -10,3 +10,11 @@ source ./env/bin/activate
 
 echo "Installing Requirements"
 pip install -r requirements.txt
+
+echo "Adding repos to lib"
+mkdir lib
+cd lib
+git clone https://github.com/anmolsjoshi/KerasHelpers.git
+git clone https://github.com/glorotxa/Shapeset.git
+cp KerasHelpers/__init__.py ..
+cp KerasHelpers/__init__.py Shapeset/
